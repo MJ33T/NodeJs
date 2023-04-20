92da41eed40557bd0cb3e59130ea50fc5c12c989
Md. Muhtasim Jeet <>
2023-04-20 17:06:00 +0600

    File Crud Complete

---
+++ data.js
@@ -0,0 +1,10 @@
+const data = [
+    {name: 'Muhtasim Jeet', email: 'jeet@test.com', contact: '01245'},
+    {name: 'Peter', email: 'peter@test.com'},
+    {name: 'Parker', email: 'parker@test.com'},
+    {name: 'James', email: 'james@test.com'},
+    {name: 'Hetfield', email: 'hetfield@test.com'},
+    
+]
+
+module.exports = data; 
--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+const fs = require('fs');
+const path = require('path');
 
-const chalk = require('chalk');
+const dirPath = path.join(__dirname, 'crud');
 
-console.log(chalk.green("Hello "));
+const filePath = `${dirPath}/crud.txt`;
+
+// fs.writeFileSync(filePath, "This is a Simple Text File");
+
+// fs.readFile(filePath,'utf8', (err, item)=>{
+//     console.log(item);
+// })
+
+
+// fs.appendFile(filePath, ' and file name is crud.txt', (err) => {
+//     if(!err) console.log("File is updated");
+// });
+
+// fs.rename(filePath, `${dirPath}/crudTest.txt`, (err)=>{
+//     if(!err) console.log("File is Renamed");
+// })
+
+fs.unlinkSync(`${dirPath}/crudTest.txt`);
+
+
+// for(i=0; i<5; i++){
+//     fs.writeFileSync(dirPath+"/demo"+i+".txt", "Demo Text for demo"+i+".txt File");
+// }
+
+// fs.writeFileSync('apple.txt', 'This is a Apple File');
+
+// fs.readdir(dirPath, (err, files)=>{
+//     files.forEach((item)=>{
+//         console.log("File name is "+item);
+//     })
+// });
+
 
 
 
